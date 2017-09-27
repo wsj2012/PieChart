0ebc673f4f1ec05801d4f65451719993f89b5e32
王树军(金融壹账通客户端研发团队) <WANGSHUJUN566@pingan.com.cn>
2017-09-27 15:31:02 +0800

    update

--- PieChart/ViewController.m
+++ PieChart/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "PieView.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSArray *percentArr = @[@0.3,@0.3,@0.1,@0.3];
+    NSArray *titleArr = @[@"现金类",@"固定收益类",@"股票类", @"基金类"];
+    NSArray *subTitleArr = @[@"30%",@"30%",@"10%", @"30%"];
+    PieView *pieView = [[PieView alloc] initWithFrame:CGRectMake(0, 64, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.width)];
+    [pieView prepareWithPercentArr:percentArr titleArr:titleArr subTitleArr:subTitleArr];
+    [self.view addSubview:pieView];
 }
 
 
